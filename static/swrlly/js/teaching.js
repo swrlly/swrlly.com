381dc928e49c4333e14263f5916619dc2edd8fdb
swrlly <25042344+swrlly@users.noreply.github.com>
2022-09-16 06:07:05 -0700

    changes

--- static/swrlly/js/teaching.js
+++ static/swrlly/js/teaching.js
@@ -1,6 +1,6 @@
 // does not account for zooming out less than 100%
 function ModifyIconFont() {
-    let icons = document.querySelectorAll(".centertextoverimage");
+    let icons = document.querySelectorAll(".center-text-over-image");
     for (let i = 0; i < icons.length; i++) {
         if (window.innerWidth >= 1200) {
             icons[i].style["font-size"] = "40px";
@@ -9,7 +9,45 @@ function ModifyIconFont() {
             icons[i].style["font-size"] = "3.448275862069vw";
         }
     }
-    icons = document.querySelectorAll(".centertextoverimage");
+    icons = document.querySelectorAll(".center-text-over-image");
+    for (let i = 0; i< icons.length; i++) {
+        let boxHeight = icons[i].offsetHeight;
+        let boxWidth = icons[i].offsetWidth;
+        let backgroundBoxHeight = icons[i].previousElementSibling.offsetHeight;
+        let backgroundBoxWidth = icons[i].previousElementSibling.offsetWidth;
+        icons[i].style.top = ((backgroundBoxHeight - boxHeight) / 2) + "px";
+        icons[i].style.left = ((backgroundBoxWidth - boxWidth) / 2) + "px";
+    }
+
+    icons = document.querySelectorAll(".large-center-text-over-image");
+    for (let i = 0; i < icons.length; i++) {
+        if (window.innerWidth >= 1200) {
+            icons[i].style["font-size"] = "80px";
+        }
+        else {
+            icons[i].style["font-size"] = "6.896551724138vw";
+        }
+    }
+    icons = document.querySelectorAll(".large-center-text-over-image");
+    for (let i = 0; i< icons.length; i++) {
+        let boxHeight = icons[i].offsetHeight;
+        let boxWidth = icons[i].offsetWidth;
+        let backgroundBoxHeight = icons[i].previousElementSibling.offsetHeight;
+        let backgroundBoxWidth = icons[i].previousElementSibling.offsetWidth;
+        icons[i].style.top = ((backgroundBoxHeight - boxHeight) / 2) + "px";
+        icons[i].style.left = ((backgroundBoxWidth - boxWidth) / 2) + "px";
+    }
+    
+    icons = document.querySelectorAll(".small-center-text-over-image");
+    for (let i = 0; i < icons.length; i++) {
+        if (window.innerWidth >= 1200) {
+            icons[i].style["font-size"] = "20px";
+        }
+        else {
+            icons[i].style["font-size"] = "1.7241379310345vw";
+        }
+    }
+    icons = document.querySelectorAll(".small-center-text-over-image");
     for (let i = 0; i< icons.length; i++) {
         let boxHeight = icons[i].offsetHeight;
         let boxWidth = icons[i].offsetWidth;
