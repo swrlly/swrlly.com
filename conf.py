d0f2b4c75038c81a07d49c5178a06e395c9074e8
swrlly <25042344+swrlly@users.noreply.github.com>
2022-09-18 02:05:23 +0000

    refresh

--- conf.py
+++ conf.py
@@ -1,3 +1,4 @@
 workers = 10
 bind = "127.0.0.1:8000"
-
+access_log_format = '%({CF-Connecting-IP}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+accesslog = "/home/swrlly/swrlly.com/access.log"
