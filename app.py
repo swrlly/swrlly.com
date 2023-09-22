3baeb1eea74d15e7fa62f97343252cccaba345e0
swrlly <25042344+swrlly@users.noreply.github.com>
2023-09-22 06:44:04 -0700

    hw 1

--- app.py
+++ app.py
@@ -104,5 +104,4 @@ def bindex():
 
 app.register_blueprint(darzacharts)
 app.register_blueprint(blog)
-#app.config["SERVER_NAME"] = "swrlly.com"
-#app.config["SERVER_NAME"] = "asfdljkadsjkl.com:8000"
+app.config["SERVER_NAME"] = "swrlly.com"
